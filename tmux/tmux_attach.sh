c49712a2cc0873b01acb7f550a5d76d305f261a2
Yuzhang Hu <hyzwowtools@gmail.com>
2020-05-02 22:42:25 -0700

    tmux: auto reattach excludes st terminal

--- tmux/tmux_attach.sh
+++ tmux/tmux_attach.sh
@@ -1,6 +1,6 @@
 # TMUX - automatically attach the first session if not inside tmux and vim
 if which tmux >/dev/null 2>&1; then
-  if [ -z $VIMRUNTIME ] && [ $TERM != "linux" ]; then
+  if [ -z $VIMRUNTIME ] && [ $TERM != "linux" ] && [[ ! $TERM =~ "st" ]]; then
     ID="$(tmux ls | head -1 | cut -d: -f1 )" # get the id of the first one
 
     if [[ -z "$ID" ]]; then # if not available create a new one
