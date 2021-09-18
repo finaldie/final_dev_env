9eaabe89dc9ccd0b260e08b6b4aa0fb598afd53f
Yuzhang Hu <hyzwowtools@gmail.com>
2021-09-18 01:36:20 -0700

    tmux: Fix ssh command matching issue

--- tmux/nw_ttl.sh
+++ tmux/nw_ttl.sh
@@ -2,7 +2,7 @@
 
 pane_tty=`tmux display -p "#{pane_tty}" | cut -d "/" -f3,4`
 
-target_host=`ps -af | grep $pane_tty | grep -v grep | grep -oP "ssh [a-zA-Z0-9.@\-]+" | cut -d " " -f2 | grep -oP "(?=@*)[\w\d.\-]*" | tail -1`
+target_host=`ps -af | grep $pane_tty | grep -v grep | grep -oP "ssh [a-zA-Z0-9.@\-]+" | grep -vP "\ \-o" | cut -d " " -f2 | grep -oP "(?=@*)[\w\d.\-]*" | tail -1`
 
 # echo "tty: $pane_tty , host: $target_host"
 
