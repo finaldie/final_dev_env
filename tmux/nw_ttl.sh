f875a1ca8c05cfedc679cec4428a53085f2b5bc1
Yuzhang Hu <hyzwowtools@gmail.com>
2021-09-18 20:29:48 -0700

    tmux: fix remote_hostname.sh

--- tmux/nw_ttl.sh
+++ tmux/nw_ttl.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-pane_tty=`tmux display -p "#{pane_tty}" | cut -d "/" -f3,4`
-
-target_host=`ps -af | grep $pane_tty | grep -v grep | grep -oP "ssh [a-zA-Z0-9.@\-]+" | grep -vP "\ \-o" | cut -d " " -f2 | grep -oP "(?=@*)[\w\d.\-]*" | tail -1`
+target_host=`bash ~/.tmux/remote_hostname.sh`
 
 # echo "tty: $pane_tty , host: $target_host"
 
--- tmux/remote_hostname.sh
+++ tmux/remote_hostname.sh
@@ -2,7 +2,7 @@
 
 pane_tty=`tmux display -p "#{pane_tty}" | cut -d "/" -f3,4`
 
-target_host=`ps -af | grep $pane_tty | grep -v grep | grep -oP "ssh [a-zA-Z0-9.@\-]+" | grep -vP "\ \-o" | cut -d " " -f2 | grep -oP "(?=@*)[\w\d.\-]*" | tail -1`
+target_host=`ps -af | awk -v ptty=$pane_tty '{if ($6 == ptty) print $0}' | grep -v grep | grep -oP "ssh [a-zA-Z0-9.@\-]+" | grep -vP "\ \-\w" | cut -d " " -f2 | grep -oP "(?=@*)[\w\d.\-]*" | tail -1`
 
 # echo "tty: $pane_tty , host: $target_host"
 
